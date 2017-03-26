6d6708693846125afa04912369bdbc1c3fef7bae
Eldad7 <eldad.corem@gmail.com>
2017-03-26 12:57:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Work in progress - Advanced Android Workshop project
+Eldad C
---
+++ app/src/main/java/eldad/corem/com/advancedandroidworkshop/MainActivity.java
@@ -0,0 +1,13 @@
+package eldad.corem.com.advancedandroidworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
